48d64bdac2b523bd996c76bb56a4e967805a3770
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2024-02-21 01:46:57 +0800

    finish login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,33 +10,37 @@
   let errorMessage = '';
 </script>
 
-<div>
-  <h1>Hello!</h1>
-  <h2>Make your cashier easy!</h2>
+<div class="bg-slate-200 h-screen">
+  <div class="flex self-center h-screen justify-center items-center">
+    <div class="bg-slate-50 gap-y-14 flex flex-col py-24 px-20 rounded-lg">
+      <div>
+        <h1 class="text-center">Hello!</h1>
+        <h2>Make your cashier easy!</h2>
+      </div>
+  
+      <form 
+        method="POST"
+        action="?/login"
+        class="flex flex-col gap-y-14"
+        use:enhance={() => {
+          return async({result}) => {
+            if (result.type === 'redirect') {
+              goto(result.location);
+            } else if (result.type === 'failure') {
+              errorMessage = result.data?.errorMessage;
+            }
+          }
+        }}>
+        <div class="flex flex-col gap-y-6">
+          <input placeholder="Username" type="text" name="username" bind:value={username}>
+          <input placeholder="Password" type="password" name="password" bind:value={password}>
+        </div>
+      
+        <div>
+          <p class="text-red-500 text-center text-s">{errorMessage}</p>
+          <button class="bg-black text-white w-full py-3 rounded-md" type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
-
-<form 
-  method="POST"
-  action="?/login"
-  use:enhance={() => {
-    return async({result}) => {
-      console.log(result);
-      if (result.type === 'redirect') {
-        goto(result.location);
-      } else if (result.type === 'failure') {
-        errorMessage = result.data?.errorMessage;
-      }
-    }
-  }}>
-  <div>
-    <input type="text" name="username" bind:value={username}>
-    <input type="password" name="password" bind:value={password}>
-  </div>
-
-  <div>
-    {#if errorMessage !== ''}
-      <p>{errorMessage}</p>
-    {/if}
-    <button type="submit">Submit</button>
-  </div>
-</form>
